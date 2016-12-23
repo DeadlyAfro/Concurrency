873d4541d45efb3b3d7bd1c7f13f2366f48d732f
DeadlyAfro <iejundt@live.nl>
2016-12-23 16:37:40 +0100

    Opzet netwerk

---
+++ MultiClientServer/MultiClientServer/bin/Debug/Standard.bat
@@ -0,0 +1,6 @@
+start MultiClientServer 11000 11020 11010
+start MultiClientServer 11010 11000 11020
+start MultiClientServer 11020 11000 11010 11060 11040
+start MultiClientServer 11040 11020 11050
+start MultiClientServer 11050 11040 11060
+start MultiClientServer 11060 11050 11020
